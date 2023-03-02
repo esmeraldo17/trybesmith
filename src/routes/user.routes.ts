28db674cc086fe91c146ece9c98b1fd1995d3719
esmeraldo17 <esmeraldoalberto55@gmail.com>
2023-03-02 20:17:32 +0100

    feat: validacoes de user criada

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,10 +1,21 @@
 import { Router } from 'express';
 import UserController from '../controllers/user.controller';
+import levelMiddlewarer from '../Middlewares/levelMiddlewarer';
+import passwordMiddlewarer from '../Middlewares/passwordMiddlewarer';
+import usernameMiddlewarer from '../Middlewares/usernameMiddlewarer';
+import vocationMiddlewarer from '../Middlewares/vocationMiddlewarer';
 
 const userController = new UserController();
 
 const router = Router();
 
-router.post('/users', userController.create);
+router.post(
+  '/users',
+  usernameMiddlewarer,
+  vocationMiddlewarer,
+  levelMiddlewarer,
+  passwordMiddlewarer,
+  userController.create,
+);
 
 export default router;
