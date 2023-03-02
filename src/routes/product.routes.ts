3b2d04e2e41d91a7d29738f1b1f141b7a60cac35
esmeraldo17 <esmeraldoalberto55@gmail.com>
2023-03-02 20:02:47 +0100

    feat: validacoes de produtos criada

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,11 +1,18 @@
 import { Router } from 'express';
 import ProductController from '../controllers/product.controller';
+import nameMiddlewarer from '../Middlewares/nameMiddlewarer';
+import amountMiddlewarer from '../Middlewares/amountMiddlewarer';
 
 const productsController = new ProductController();
 
 const router = Router();
 
 router.get('/products', productsController.getAll);
-router.post('/products', productsController.create);
+router.post(
+  '/products',
+  nameMiddlewarer,
+  amountMiddlewarer,
+  productsController.create,
+);
 
 export default router;
